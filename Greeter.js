2ebd8836bfa9ddf30689352e42f27cf3d4136d34
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-04-20 15:35:07 +0530

    setup default values for the parameters

--- Greeter.js
+++ Greeter.js
@@ -2,4 +2,12 @@
     let Greeter = function(firstName, lastName, language) {
         // return new Greeter.init(firstName, lastName, language);
     }
-}(window, $))
+
+    Greeter.init = function(firstName, lastName, language) {
+        let self = this;
+
+        self.firstName = firstName || "";
+        self.lastName = lastName || "";
+        self.language = language || "en";
+    }
+}('window', '$'))
