4a91af6a92242fc307769844baaf1838866f7a42
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-04-20 15:41:17 +0530

    Create prototype to write methods

--- Greeter.js
+++ Greeter.js
@@ -3,6 +3,9 @@
         // return new Greeter.init(firstName, lastName, language);
     }
 
+    // This is where we put the methods we need to use inside the Greeter object
+    Greeter.prototype = {}
+
     Greeter.init = function(firstName, lastName, language) {
         let self = this;
 
@@ -10,4 +13,8 @@
         self.lastName = lastName || "";
         self.language = language || "en";
     }
+
+    // Greeter.init.prototype is returned on Greeter initialization. 
+    // Make the Greeter objects's methods available in Greeter initialization ( init method proto == making main obj proto)
+    Greeter.init.prototype = Greeter.prototype;
 }('window', '$'))
