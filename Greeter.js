f5158eb52284e8ae07144adc01f9d1ae1c5327ff
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-04-25 13:48:58 +0530

    create chainable greeting methods

--- Greeter.js
+++ Greeter.js
@@ -3,8 +3,52 @@
         return new Greeter.init(firstName, lastName, language);
     }
 
+    let supportedLangs = ['en', 'es'];
+
+    let greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    }
+
+    let formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    }
+
+    let logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion'
+    }
+
     // This is where we put the methods we need to use inside the Greeter object
-    Greeter.prototype = {}
+    Greeter.prototype = {
+        fullName: function() {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) throw "Invalid language";
+        },
+        greeting: function() {
+            return `${greetings[this.language]} ${this.firstName}!`
+        },
+        formalGreetings: function() {
+            return `${formalGreetings[this.language]}, ${this.fullName()}`;
+        },
+        greet: function(formal) {
+            let msg = formal ? this.formalGreetings() : this.greeting();
+            if(console) console.log(msg);
+            return this;
+        },
+        log: function() {
+            if(console) console.log(`${logMessages[this.language]} : ${this.fullName()}`);
+            return this;
+        },
+        setLang: function(lang) {
+            this.language = lang;
+            this.validate();
+            return this;
+        }
+    }
 
     Greeter.init = function(firstName, lastName, language) {
         let self = this;
@@ -20,4 +64,5 @@
 
     // Make G$ (like '$' in jquery) available in global method
     global.Greeter = global.G$ = Greeter;
+    
 }(window, $))
--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 let g = G$('Sahan', 'Weerakoon');
-console.log(g);
+g.greet().setLang('es').greet(true);
